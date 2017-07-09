a2e9a7a790ba693f4e15510e2f9bedb583232426
unamezzz <3126983166@qq.com>
2017-07-09 18:37:55 +0800

    百度搜索，完善标题正则和编码，增加获取301等跳转后的标题和网址

--- pa.py
+++ pa.py
@@ -8,17 +8,6 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 # 禁用安全请求警告
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-url = "https://www.baidu.com/s?wd=%s&pn=%d"
-wd = raw_input("输入查询关键字：")
-wd = quote(wd)
-pn = raw_input("输入需要采集的条数：")
-pn = int(pn)
-
-print
-
-count = 1
-pn10 = 0
-
 
 def page1(url , pn , count):
     headers = {"User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0"}
@@ -27,18 +16,57 @@ def page1(url , pn , count):
     pattern = '''<h3 class="t"><a [\s\S]*?href = "(.*?)"[\s\S]*?>([\s\S]*?)</a></h3>'''
 
     for x in re.findall(pattern , response2):
-        if count<=pn:
-            result = x[1].replace('<em>','').replace('</em>','')
-            print count,result
+        if count<=pn:#已爬数目小于需要采集条数
+            result = x[1].replace('<em>','').replace('</em>','').replace(' ','')
             url2 = x[0]
-            response2 = requests.head(url2 , verify=False)
-            print response2.headers['Location']
+            response3 = requests.head(url2 , verify=False)
+            url3 = response3.headers['Location']
+            #得到真实网址url3
+            
+            try:
+                response4 = requests.get(url3,verify=False,headers=headers)
+                if response4.status_code==200:
+                    print count,
+                    if url3 != response4.url:#判断真实网址后面是否有做301等跳转
+                        print result
+                        print url3
+                    try:
+                        title = re.findall("<title>([\s\S]*?)</title>",response4.content)[0]
+                        try:
+                            title = title.decode('UTF-8')
+                        except:
+                            pass
+                        print title.replace('\n','').replace('\t','').replace(' ','')
+                    except:
+                        print "正则匹配title失败，百度搜索结果为：",result
+                    print response4.url
+                else:
+                    print count,resul
+                    print url3
+                    print response4.status_code,"已死"
+            except:
+                print result
+                print url3
+                print "请求真实网址失败"
             print
             count += 1
         else:
             return 0
-
     return count
+
+
+url = "https://www.baidu.com/s?wd=%s&pn=%d"
+wd = raw_input("输入查询关键字：")
+wd = quote(wd)
+pn = raw_input("输入需要采集的条数：")
+pn = int(pn)
+
+count = 1
+#目前已爬条数
+pn10 = 0
+#当前页数，每10条为一页
+
+print
 
 while count <= pn:
     url2 = url%(wd,pn10)
